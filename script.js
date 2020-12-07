5717f125154e4ec10402dcc5e7de7e4506c6d182
Lmaobeh <lucasfnguyen14@gmail.com>
2020-12-06 21:16:01 -0600

    2 files modified, 1 added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('hello!')
+
 let myWindow;
 function openNewWindow(button){
   myWindow = window.open("", "_self");
@@ -6,7 +6,16 @@ function openNewWindow(button){
 
  
 }
-function load(name){
-  console.log(name);
+
+function getPersonInputString(){
+  let name = document.getElementById("personName").value;
+  if(name.length === 0){
+    document.getElementById("errorMsg").innerHTML = "please enter a name";
+    return;
+  }
+   document.querySelectorAll("input").forEach(
+          (input_tag) => {input_tag.disabled ="true";}
+          );
+  myWindow = window.open("", "_self");
+  myWindow.document.write("<p>"+name+"</p><a href=\"index.html\">back</a>")
 }
-myWindow.onload = console.log('hello');
